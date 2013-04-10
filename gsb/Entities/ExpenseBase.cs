cf7199dbe95cf1cc6180f80749294246e9a194ca
Johann Pardanaud <pardanaud.j@gmail.com>
2013-04-10 22:20:19 +0200

    ExpenseBase class added

---
+++ gsb/Entities/ExpenseBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.Common;
+
+namespace gsb.Entities
+{
+    abstract class ExpenseBase
+    {
+        public abstract void Fill(DbDataReader row);
+        public abstract bool Save();
+
+        // Forces the overriding of the ToString() method
+        public abstract override string ToString();
+    }
+}
