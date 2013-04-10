3720912aa328d13abfc1a29683f9515876359671
Johann Pardanaud <pardanaud.j@gmail.com>
2013-04-10 23:34:40 +0200

    ExpenseNote entity can now be filled

--- gsb/Entities/ExpenseNote.cs
+++ gsb/Entities/ExpenseNote.cs
@@ -1,8 +1,55 @@
 ﻿using System;
+using System.Data.Common;
 
 namespace gsb.Entities
 {
-    class ExpenseNote
+    class ExpenseNote : ExpenseBase
     {
+        /*
+         * Fields
+         */
+
+        private bool newEntity = true;
+        private bool modified = false;
+
+        #region Data fields
+        private DateTime date;
+        private int vouchersNb;
+        private decimal approvedAmount;
+        private string state;
+        #endregion
+
+        /*
+         * Constructors
+         */
+
+        public ExpenseNote(DbDataReader row)
+        {
+            this.Fill(row);
+        }
+
+        /*
+         * Methods
+         */
+
+        public override void Fill(DbDataReader row)
+        {
+            this.newEntity = false;
+
+            this.date = (DateTime)row["date"];
+            this.vouchersNb = (int)row["vouchers"];
+            this.approvedAmount = (decimal)row["amount"];
+            this.state = (string)row["state"];
+        }
+
+        public override bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
