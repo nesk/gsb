b94ddb792e5a1ec9cd54b80794b6106abdf64884
Johann Pardanaud <pardanaud.j@gmail.com>
2013-04-13 18:39:19 +0200

    The ExpenseOffPlan class can now be filled

--- gsb/Entities/ExpenseOffPlan.cs
+++ gsb/Entities/ExpenseOffPlan.cs
@@ -1,8 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace gsb.Entities
 {
-    class ExpenseOffPlan
+    class ExpenseOffPlan : ExpenseBase
     {
+        /*
+         * Fields
+         */
+
+        private ExpenseState status = ExpenseState.New;
+
+        #region Data fields
+        private int id;
+        private string label;
+        private DateTime date;
+        private decimal cost;
+        #endregion
+
+        /*
+         * Constructors
+         */
+
+        public ExpenseOffPlan(Dictionary<string, object> row)
+        {
+            this.Fill(row);
+        }
+
+        /*
+         * Methods
+         */
+
+        public override void Fill(Dictionary<string, object> row)
+        {
+            this.status = ExpenseState.Loaded;
+
+            this.id = (int)row["id"];
+            this.label = (string)row["label"];
+            this.date = (DateTime)row["date"];
+            this.cost = (decimal)row["cost"];
+        }
+
+        public override bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
